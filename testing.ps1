489a47f73b8db570926495fd44f9556f13bb37ff
toteb <toteb@abv.bg>
2022-03-05 16:39:51 +0200

    comments to functions

--- testing.ps1
+++ testing.ps1
@@ -1,9 +1,2 @@
-powershell.exe -command "& {'C:\Users\nikolay\github files\files\Foreach-ObjectFast.ps1'}"
 
-# Where-Object is just....
-$r1 = Get-Service | Where-Object { $_.Status -eq 'Running' }
 
-# a special case of Foreach-Object
-$r2 = Get-Service | Foreach-Object { if ($_.Status -eq 'Running') { $_ } }
-
-Compare-Object -ReferenceObject $r1 -DifferenceObject $r2 -IncludeEqual
