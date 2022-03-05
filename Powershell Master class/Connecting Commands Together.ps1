aa3755bba54454815c39508a62f2a85f5b9731bb
toteb <toteb@abv.bg>
2022-03-05 19:23:43 +0200

    Confirm

--- Powershell Master class/Connecting Commands Together.ps1
+++
@@ -1,8 +0,0 @@
-# Get-Member (gm)- > shows all properties of Object "Get-Process"
-Get-Process *a | Get-Member
-
-# Keep the objects as long as we can . Object can be manipulated. Strings can't.
-
-#Adding custom property to select-object : 
-
-get-process | Select-Object -First 5 -Property name, @{name='procId';expression={$_.id}}
